a55161ee2bfcf9dffca1836c58a5d4c76b3edeb4
alanerochaa <alanerochaa@gmail.com>
2025-06-05 22:56:32 -0300

    add RedirectController

---
+++ src/main/java/com/humanlink/exception/RouteNotFoundException.java
@@ -0,0 +1,22 @@
+package com.humanlink.exception;
+
+import com.humanlink.exception.dto.ResponseDTO;
+import jakarta.ws.rs.NotFoundException;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import java.time.LocalDateTime;
+
+@Provider
+public class RouteNotFoundException implements ExceptionMapper<NotFoundException> {
+    @Override
+    public Response toResponse(NotFoundException exception) {
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(ResponseDTO.builder()
+                        .mensagem("Route not found")
+                        .status(Response.Status.NOT_FOUND.getStatusCode())
+                        .dataErro(LocalDateTime.now())
+                        .build())
+                .build();
+    }
+}
