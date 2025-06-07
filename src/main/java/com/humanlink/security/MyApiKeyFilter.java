55323c4e06d2977eacda88c9f465730c1cfd4f0d
alanerochaa <alanerochaa@gmail.com>
2025-06-07 14:49:26 -0300

    Implementacao do filtro de API Key e configurar OpenAPI para autenticação via header

--- src/main/java/com/humanlink/security/MyApiKeyFilter.java
+++ src/main/java/com/humanlink/security/MyApiKeyFilter.java
@@ -4,8 +4,10 @@ import jakarta.annotation.Priority;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import java.io.IOException;
 
 @Provider
@@ -18,7 +20,13 @@ public class MyApiKeyFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         String apiKeyRequest = requestContext.getHeaderString("X-API-key");
+
+        if (apiKeyRequest == null || !apiKeyRequest.equals(apiKey)) {
+            requestContext.abortWith(
+                    Response.status(Response.Status.UNAUTHORIZED)
+                            .entity("API Key inválida ou ausente")
+                            .build()
+            );
+        }
     }
 }
-
-
