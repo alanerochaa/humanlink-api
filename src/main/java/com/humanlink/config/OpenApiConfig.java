aa86144eb46a69e52a3b56893bb5c06a1721bc04
alanerochaa <alanerochaa@gmail.com>
2025-06-07 19:28:26 -0300

    atualizacoes no application.properties

--- src/main/java/com/humanlink/config/OpenApiConfig.java
+++ src/main/java/com/humanlink/config/OpenApiConfig.java
@@ -1,5 +1,6 @@
 package com.humanlink.config;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeIn;
@@ -7,6 +8,7 @@ import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
+@ApplicationScoped
 @OpenAPIDefinition(
         info = @Info(title = "HumanLink API", version = "1.0"),
         security = @SecurityRequirement(name = "apiKey")
@@ -15,7 +17,7 @@ import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
         securitySchemeName = "apiKey",
         type = SecuritySchemeType.APIKEY,
         in = SecuritySchemeIn.HEADER,
-        apiKeyName = "X-API-Key",
+        apiKeyName = "X-API-key",  // mantém o padrão do header em lowercase no "key"
         description = "API Key necessária para autenticação"
 )
 public class OpenApiConfig {
