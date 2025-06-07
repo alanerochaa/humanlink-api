832dbc82f83dbdabfbc8de0b40ad138f50a49712
alanerochaa <alanerochaa@gmail.com>
2025-06-07 15:50:04 -0300

    Atualizacao

--- src/main/java/com/humanlink/security/MyApiKeyFilter.java
+++ src/main/java/com/humanlink/security/MyApiKeyFilter.java
@@ -4,10 +4,8 @@ import jakarta.annotation.Priority;
 import jakarta.ws.rs.Priorities;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
-import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import java.io.IOException;
 
 @Provider
@@ -20,13 +18,5 @@ public class MyApiKeyFilter implements ContainerRequestFilter {
     @Override
     public void filter(ContainerRequestContext requestContext) throws IOException {
         String apiKeyRequest = requestContext.getHeaderString("X-API-key");
-
-        if (apiKeyRequest == null || !apiKeyRequest.equals(apiKey)) {
-            requestContext.abortWith(
-                    Response.status(Response.Status.UNAUTHORIZED)
-                            .entity("API Key inválida ou ausente")
-                            .build()
-            );
-        }
     }
 }
